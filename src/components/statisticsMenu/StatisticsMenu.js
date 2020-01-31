9e1ede9a7aca32853db8779780d313d219d94460
Pavlova <irochkapavlova85@gmail.com>
2020-01-31 20:09:44 +0200

    edit dateNow, add class

--- src/components/statisticsMenu/StatisticsMenu.js
+++ src/components/statisticsMenu/StatisticsMenu.js
@@ -1,40 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './statisticsMenu.module.css';
+import getDateNow from '../../utilities/getDateNow';
 
-const StatisticsMenu = () => (
-  <div className={styles.statistics}>
-    <div className={styles.statisticsMenu}>
-      <button className={styles.statisticsMenu_goBack} type="button">
-        Вернуться
-        <span className={styles.statisticsMenu_goBack_span}> на главную</span>
-      </button>
-      <div className={styles.statisticsMenu_dateBalance_wrap}>
-        <p className={styles.statisticsMenu_dateBalance_text}>
-          Баланс на 23.01.2020:
-        </p>
-        <p className={styles.statisticsMenu_dateBalance_value}>
-          150,000.00 грн
-        </p>
-      </div>
-      <div className={styles.statisticsMenu_calendar_wrap}>
-        <p className={styles.statisticsMenu_calendar_text}>Текущий период:</p>
-        <div className={styles.statisticsMenu_calendar_monthsWrap}>
+class StatisticsMenu extends Component {
+  state = {};
+
+  //handleGoBack = () => {
+  //this.props.history.push('/dashboard');
+  //this.props.history.goBack();
+  //};
+
+  render() {
+    return (
+      <div className={styles.statistics}>
+        <div className={styles.statisticsMenu}>
           <button
-            className={styles.statisticsMenu_calendar_monthsBtn_left}
+            className={styles.statisticsMenu_goBack}
             type="button"
-            name="leftBtn"
-          ></button>
-          <p className={styles.statisticsMenu_calendar_months}>Январь 2020</p>
-          <button
-            className={styles.statisticsMenu_calendar_monthsBtn_right}
-            type="button"
-            name="rightBtn"
-          ></button>
+            onClick={this.handleGoBack}
+          >
+            Вернуться
+            <pre className={styles.statisticsMenu_goBack_pre}> на главную</pre>
+          </button>
+          <div className={styles.statisticsMenu_dateBalance_wrap}>
+            <p className={styles.statisticsMenu_dateBalance_text}>
+              Баланс на {getDateNow()}:
+            </p>
+            <p className={styles.statisticsMenu_dateBalance_value}>
+              150,000.00 грн
+            </p>
+          </div>
+          <div className={styles.statisticsMenu_calendar_wrap}>
+            <p className={styles.statisticsMenu_calendar_text}>
+              Текущий период:
+            </p>
+            <div className={styles.statisticsMenu_calendar_monthsWrap}>
+              <button
+                className={styles.statisticsMenu_calendar_monthsBtn_left}
+                type="button"
+                name="leftBtn"
+              ></button>
+              <p className={styles.statisticsMenu_calendar_months}>
+                Январь 2020
+              </p>
+              <button
+                className={styles.statisticsMenu_calendar_monthsBtn_right}
+                type="button"
+                name="rightBtn"
+              ></button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default StatisticsMenu;
