fff17e93fb1aaff205b3b81f25d3c90f9947c3fe
Pavlova <irochkapavlova85@gmail.com>
2020-02-01 12:40:57 +0200

    DashboardPanel add css for children

--- src/components/Dashboard/dashboardPanel/DashboardPanel.js
+++ src/components/Dashboard/dashboardPanel/DashboardPanel.js
@@ -2,16 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './dashboardPanel.module.css';
 import TableExample from '../summary/summary';
+import AddCost from '../../addCost/AddCost';
 
 const DashboardPanel = () => (
   <div className={styles.dashboardPanel}>
-    <button className={styles.dashboardPanelBtnMobile} type="button">
-      Ввести расход
-    </button>
-    <div className={styles.dashboardPanel_Olya}></div>
-    <div className={styles.dashboardPanel_Bogdan}></div>
-    <div className={styles.dashboardPanel_Den}>
-      <TableExample />
+    {window.innerWidth < 768 ? (
+      <button className={styles.dashboardPanelBtnMobile} type="button">
+        Ввести расход
+      </button>
+    ) : (
+      <div className={styles.dashboardPanel_addCost}>
+        <AddCost />
+      </div>
+    )}
+    <div className={styles.dashboardPanel_wrap}>
+      <div className={styles.dashboardPanel_Bogdan}></div>
+      <div className={styles.dashboardPanel_tableExample}>
+        <TableExample />
+      </div>
     </div>
   </div>
 );
