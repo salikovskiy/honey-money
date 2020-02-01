8fa801d3d028e8392c4c9a827beba55d07c560a1
Pavlova <irochkapavlova85@gmail.com>
2020-02-01 17:05:09 +0200

    add momemt, changeMonth

--- src/components/statisticsMenu/StatisticsMenu.js
+++ src/components/statisticsMenu/StatisticsMenu.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './statisticsMenu.module.css';
-import getDateNow from '../../utilities/getDateNow';
+import moment from 'moment';
+import 'moment/locale/ru';
+
+moment.locale('ru');
+const dateNow = moment().format();
 
 class StatisticsMenu extends Component {
-  state = {};
+  state = {
+    date: dateNow,
+    dateRegistration: '2019-09-30T18:22:59.462Z',
+  };
 
-  //handleGoBack = () => {
-  //this.props.history.push('/dashboard');
-  //this.props.history.goBack();
-  //};
+  // handleGoBack = () => {
+  //   //this.props.history.push('/dashboard');
+  //   //this.props.history.goBack();
+  // };
+
+  handleMonthChange = e => {
+    if (e.target.name === 'leftBtn') {
+      this.setState({
+        date: moment(this.state.date)
+          .add(-1, 'month')
+          .format(),
+      });
+    }
+    if (e.target.name === 'rightBtn') {
+      this.setState({
+        date: moment(this.state.date)
+          .add(+1, 'month')
+          .format(),
+      });
+    }
+  };
+
+  handlePrevMonth = () => {
+    if (moment(this.state.date).isAfter(this.state.dateRegistration)) {
+      return false;
+    } else return true;
+  };
+
+  handleNextMonth = () => {
+    if (
+      moment().format('MMMM YYYY') ===
+      moment(this.state.date).format('MMMM YYYY')
+    ) {
+      return true;
+    } else return false;
+  };
 
   render() {
     return (
@@ -25,7 +64,7 @@ class StatisticsMenu extends Component {
           </button>
           <div className={styles.statisticsMenu_dateBalance_wrap}>
             <p className={styles.statisticsMenu_dateBalance_text}>
-              Баланс на {getDateNow()}:
+              Баланс на {moment().format('DD.MM.YYYY')}:
             </p>
             <p className={styles.statisticsMenu_dateBalance_value}>
               150,000.00 грн
@@ -37,17 +76,29 @@ class StatisticsMenu extends Component {
             </p>
             <div className={styles.statisticsMenu_calendar_monthsWrap}>
               <button
-                className={styles.statisticsMenu_calendar_monthsBtn_left}
+                className={
+                  this.handlePrevMonth()
+                    ? styles.statisticsMenu_calendar_monthsBtn_left_dis
+                    : styles.statisticsMenu_calendar_monthsBtn_left
+                }
                 type="button"
                 name="leftBtn"
+                onClick={this.handleMonthChange}
+                disabled={this.handlePrevMonth()}
               ></button>
               <p className={styles.statisticsMenu_calendar_months}>
-                Январь 2020
+                {moment(this.state.date).format('MMMM YYYY')}
               </p>
               <button
-                className={styles.statisticsMenu_calendar_monthsBtn_right}
+                className={
+                  this.handleNextMonth()
+                    ? styles.statisticsMenu_calendar_monthsBtn_right_dis
+                    : styles.statisticsMenu_calendar_monthsBtn_right
+                }
                 type="button"
                 name="rightBtn"
+                onClick={this.handleMonthChange}
+                disabled={this.handleNextMonth()}
               ></button>
             </div>
           </div>
