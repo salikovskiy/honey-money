6df3791f6b30f6fb19801726fed79a575755d05b
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2020-02-02 22:11:14 +0200

    misha

--- src/components/DashboardMenu/DashboardMenu.js
+++ src/components/DashboardMenu/DashboardMenu.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import css from './dashboardMenu.module.css';
 
-const DashboardMenu = () => {
+const DashboardMenu = ({ props }) => {
   return (
     <div className={css.dashMenu__container}>
-      <form method="post" className={css.balance__form}>
-        <label className={css.balance__label}>БАЛАНС:</label>
-        <input className={css.balance__input} placeholder="00.00 грн"></input>
+      <form onSubmit={props} className={css.balance__form}>
+        <label htmlFor="input" className={css.balance__label}>
+          БАЛАНС:
+        </label>
+        <input
+          value={props}
+          onChange={props}
+          type="number"
+          className={css.balance__input}
+          placeholder="00.00 грн"
+          name="name"
+        ></input>
       </form>
       <button className={css.dashMenu__incomeBtn}>Ввести доход</button>
       <div className={css.dashMenu__linkContainer}>
-        <Link to="/statistics" className={css.dashMenu__linkTo}>
+        <NavLink to="/statistics" className={css.dashMenu__linkTo}>
           <svg
             style={{
               display: 'block',
@@ -24,10 +33,10 @@ const DashboardMenu = () => {
             <path d="M5 9.2h3V19H5zM10.6 5h2.8v14h-2.8zm5.6 8H19v6h-2.8z" />
             <path fill="none" d="M0 0h24v24H0z" />
           </svg>
-        </Link>
-        <Link to="/statistics" className={css.dashMenu__linkTitle}>
+        </NavLink>
+        <NavLink to="/statistics" className={css.dashMenu__linkTitle}>
           Перейти к отчётам
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
