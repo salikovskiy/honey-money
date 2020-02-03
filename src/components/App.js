601f51832dd794d86d47ede52f9b5b994fc9ba74
Romashka093 <aa.romashchenko@gmail.com>
2020-02-03 16:02:55 +0200

    init Loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-// import React from 'react';
+import React from 'react';
 import './App.css';
 import { useRoute } from '../routes';
+import Loader from './../components/Loader/LoaderThreeDots';
+import { connect } from 'react-redux';
 
-const App = () => {
+const App = props => {
   const route = useRoute(true);
-  return route;
+  const isLoading = props.finance.isLoading;
+  return (
+    <>
+      {isLoading && <Loader />}
+      {route}
+    </>
+  );
 };
+const mapStateToProps = state => state;
 
-export default App;
+export default connect(mapStateToProps)(App);
