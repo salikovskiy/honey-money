3f51714b89efc7e4bb482899b155bac9666ed1a7
Alex Yu <alex.yurchenko@gmail.com>
2020-02-03 17:36:21 +0200

    development save point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+// import App from './components/App';
+import App from './components/addIncome/App';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
