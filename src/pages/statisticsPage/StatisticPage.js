c55f96720c677a6accfd4be65b1ad64dc314d083
RBCHK <artem.rybachuk@gmail.com>
2020-02-03 14:07:20 +0200

    merge dev

--- src/pages/statisticsPage/StatisticPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const StatisticPage = () => <h2>StatisticPage</h2>;
-
-export default StatisticPage;
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import AuthPage from './pages/authPage/AuthPage';
 import DashboardPage from './pages/dashboardPage/DashboardPage';
-import StatisticPage from './pages/statisticsPage/StatisticPage';
+import StatisticsPage from './pages/StatisticsPage/StatisticsPage';
 
 export const useRoute = isAuth => {
   if (isAuth) {
     return (
       <Switch>
         <Route path="/dashboard" component={DashboardPage} />
-        <Route path="/statistics" component={StatisticPage} />
+        <Route path="/statistics" component={StatisticsPage} />
         <Redirect to="/dashboard" />
       </Switch>
     );
