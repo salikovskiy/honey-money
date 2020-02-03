69e6f671047d29ae211796ab3b854b064635f775
Rodion Pedyuk <rodionpedyuk@gmail.com>
2020-02-03 11:35:31 +0200

    init CategoriesList

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Categorieslist from './components/CategoriesList/CategoriesList';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,6 +13,7 @@ ReactDOM.render(
     <PersistGate loading={null} persistor={persistor}>
       <Router>
         <App />
+        <Categorieslist />
       </Router>
     </PersistGate>
   </Provider>,
