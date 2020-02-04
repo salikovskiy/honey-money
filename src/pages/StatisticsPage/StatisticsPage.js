e1343a7f67ea1e8ecb7ada356790f1f10726f715
RBCHK <artem.rybachuk@gmail.com>
2020-02-04 14:31:37 +0200

    merge dev

--- src/pages/StatisticsPage/StatisticsPage.js
+++ src/pages/StatisticsPage/StatisticsPage.js
@@ -4,7 +4,32 @@ import StatisticsMenu from '../../components/statisticsMenu/StatisticsMenu';
 import CategoriesList from '../../components/CategoriesList/CategoriesList';
 
 class StatisticsPage extends Component {
+  state: {
+    // Хранить в стейте какая именно категория
+    // выбрана и текущуюю дату (по ней определяется месяц)
+  };
+
+  componentDidMount() {
+    // GET запросы:
+    // 1) получение данных по категриям;
+    // 2) суммарные доходы и расходы.
+    // Полученные данные записать в стейт.
+  }
+
+  // Написать операцию (ГЕТ запрос) с измененным
+  // месяцем и текущей категорией в стейте и прокинуть
+  // в StatisticsMenu на переключение месяцев
+
+  // Из текущего массива расходов, формирует каждый
+  // раз в componentDidUpdate объект категории и
+  // суммарной сумме по данной категории и передает
+  // его пропом в ChartStatisticByCategory для
+  // отображения  (формат объекта согласовать с тем
+  //   кто будет делать ChartStatisticByCategory)
+
   render() {
+    // Компонент рендерит: StatisticMenu, StatisticAmounts,
+    // CategoriesList, Chart
     return (
       <div className={s.wrapper}>
         <div className={s.header}>Header</div>
