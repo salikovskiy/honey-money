b0cc8ab172760eb6bc44b24dbf7ef91d7c2ed363
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-02-04 22:13:58 +0200

    added initial version

---
+++ src/components/modalBackDrop/ModalBackDrop.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import styles from './ModalBackDrop.module.css';
+
+const ModalBackDrop = WrappedComponent => {
+  return class ModalBackDrop extends Component {
+    state = {
+      isOpen: this.props.isOpen === true ? true : false,
+    };
+
+    componentDidMount() {
+      document.addEventListener('keydown', this.escFunction, false);
+    }
+
+    componentWillUnmount() {
+      document.removeEventListener('keydown', this.escFunction, false);
+    }
+
+    escFunction = event => {
+      if (event.keyCode === 27) {
+        this.closeBackDrop();
+      }
+    };
+
+    outsideClick = event => {
+      const dataset = event.target.dataset;
+      if (dataset && dataset.modal === 'true') {
+        this.closeBackDrop();
+      }
+    };
+
+    closeBackDrop = () => {
+      this.setState({
+        isOpen: false
+      });
+    };
+
+    render() {
+      return !this.state.closed ? (
+        <div
+          className={styles.lightbox}
+          data-modal={'true'}
+          onClick={this.outsideClick}
+        >
+          <button className={styles.lightboxBtn} onClick={this.closeBackDrop}>
+            <i className="material-icons">close</i>
+          </button>
+          <WrappedComponent {...this.props} />
+        </div>
+      ) : null;
+    }
+  };
+};
+
+export default ModalBackDrop;
