8193df356bdfeeaace25d63e8a99e4da2167a6d8
Pavlova <irochkapavlova85@gmail.com>
2020-02-05 16:33:20 +0200

    add logic

--- src/components/Dashboard/dashboardPanel/DashboardPanel.js
+++ src/components/Dashboard/dashboardPanel/DashboardPanel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import styles from './dashboardPanel.module.css';
 import DashboardTable from '../../dashboardTable/DashboardTable';
 import TableExample from '../summary/summary';
@@ -7,6 +6,7 @@ import AddCost from '../../addCost/AddCost';
 import moment from 'moment';
 import 'moment/locale/ru';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 //let date = moment().format('YYYYMM');
 
@@ -23,7 +23,7 @@ const monthsSummary = [
 
 class DashboardPanel extends Component {
   state = {
-    //date: moment().format('YYYYMM'),
+    date: moment().format('YYYYMM'),
     dataTable: [],
   };
 
@@ -42,17 +42,27 @@ class DashboardPanel extends Component {
     return summary;
   };
 
-  handleGetDate = e => {
-    //console.log('event.target', e.target.parentElement.dataset.month);
-    // this.setState({
-    //   date: e.target.parentElement.dataset.month,
-    // });
+  componentDidMount() {
+    this.handleGetDataTable();
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.date !== this.state.date) {
+      this.handleGetDataTable();
+    }
+  }
+
+  handleGetDate = e => {
+    this.setState({
+      date: e.target.parentElement.dataset.month,
+    });
+  };
+
+  handleGetDataTable = () => {
     let x = [];
     this.props.finance.costs.map(
       elem =>
-        moment(elem.date).format('YYYYMM') ===
-          e.target.parentElement.dataset.month &&
+        moment(elem.date).format('YYYYMM') === this.state.date &&
         (x = [
           ...x,
           {
@@ -71,11 +81,13 @@ class DashboardPanel extends Component {
 
   render() {
     const balance = this.props.finance.balance;
+    const token = this.props.finance.authReducer.token;
     const dateRegistration = this.props.finance.authReducer.createdAt;
     const summary = this.handleGetSummary();
-    console.log(this.props.finance);
-
-    console.log('state data', this.state.dataTable);
+    // console.log(token);
+    // console.log(this.props.finance.authReducer);
+    // console.log('state date', this.state.date);
+    // console.log('state data', this.state.dataTable);
     return (
       <div className={styles.dashboardPanel}>
         {window.innerWidth < 768 ? (
@@ -88,6 +100,7 @@ class DashboardPanel extends Component {
               balance={balance}
               dateRegistration={dateRegistration}
               postCosts={this.props.postCosts}
+              token={token}
             />
           </div>
         )}
