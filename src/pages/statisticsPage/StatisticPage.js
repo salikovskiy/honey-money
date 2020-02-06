28af7be3bdd68a1c16338ac82e84496749e98084
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-02-06 12:11:47 +0200

    bar chart prototype

--- src/pages/statisticsPage/StatisticPage.js
+++ src/pages/statisticsPage/StatisticPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import StatisticsMenu from '../../components/statisticsMenu/StatisticsMenu';
+import BarChart from "../../components/barChart/BarChart"
+const StatisticPage = () => {
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'January', 'February', 'March'];
+  const data = [65, 59, 80, 81, 56, 55, 40, 3, 59, 80];
 
-const StatisticPage = () => (
-  <>
+  return <>
     <h2>StatisticPage</h2>
     <StatisticsMenu />
+    <BarChart labels={labels} data={data} />
   </>
-);
+};
 
 export default StatisticPage;
