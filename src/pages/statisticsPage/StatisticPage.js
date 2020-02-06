62eef87ede742f0fb7dc7557114b1f8901c41540
Rodion Pedyuk <rodionpedyuk@gmail.com>
2020-02-06 14:15:39 +0200

    init active buttons added

--- src/pages/statisticsPage/StatisticPage.js
+++ src/pages/statisticsPage/StatisticPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Categorieslist from '../../components/CategoriesList/CategoriesList';
 //import StatisticsMenu from '../../components/statisticsMenu/StatisticsMenu';
 
 const StatisticPage = () => (
   <>
     <h2>StatisticPage</h2>
+    <Categorieslist />
     {/* <StatisticsMenu /> */}
   </>
 );
