5b87f60efb2ee9fe7d087065dd2fd36b6531bc38
salikovskiy <ssalikovskiy@gmail.com>
2020-02-06 10:44:47 +0200

    header fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,39 @@
+import React from 'react';
 import { connect } from 'react-redux';
-import {logOut} from '../../redux/actions'
+import { logOut } from '../../redux/actions';
+import { Link } from 'react-router-dom';
+import style from './header.module.css';
+import svg from '../../assets/img/logo/svg/logo_146x34.svg';
 
-const Header = (props) => {
-    console.log('props', props)
-    return (
-  <div className={style.header}>
-    <Link to="/dashboard">
-      <img className={style.header_logo} src={svg} alt="logo" />
-    </Link>
-    <div className={style.header_user}>
-      {/* <p className={style.header_user_name}>User Name</p> */}
-      <button onClick={()=>props.logOut()} type="button" className={style.header_user_logout}>Выйти</button>
+const Header = props => {
+  console.log('props', props);
+  return (
+    <div className={style.header}>
+      <Link to="/dashboard">
+        <img className={style.header_logo} src={svg} alt="logo" />
+      </Link>
+      <div className={style.header_user}>
+        {/* <p className={style.header_user_name}>User Name</p> */}
+        <button
+          onClick={() => props.logOut()}
+          type="button"
+          className={style.header_user_logout}
+        >
+          Выйти
+        </button>
+      </div>
     </div>
-  </div>
-//   <div className={style.modal}>
-//     <p className={style.modal_text}>Вы действительно хотите выйти?</p>
-//     <div className={style.modal_button}>
-//     <button type="button" className={style.modal_button_yes}>Да</button>
-//     <button type="button" className={style.modal_button_no}>Нет</button>
-//     </div>
-//   </div>
-  
-)};
-const mapDispatchToProps =  {
-    logOut,
-  };
+    //   <div className={style.modal}>
+    //     <p className={style.modal_text}>Вы действительно хотите выйти?</p>
+    //     <div className={style.modal_button}>
+    //     <button type="button" className={style.modal_button_yes}>Да</button>
+    //     <button type="button" className={style.modal_button_no}>Нет</button>
+    //     </div>
+    //   </div>
+  );
+};
+const mapDispatchToProps = {
+  logOut,
+};
 
-export default connect(null, mapDispatchToProps) (Header);
+export default connect(null, mapDispatchToProps)(Header);
