c598c1b4973ebddca3e174c33dd4d0a0da455a54
Romashka093 <aa.romashchenko@gmail.com>
2020-02-06 10:26:12 +0200

    task css

--- src/components/statisticAmounts/StatisticAmounts.js
+++ src/components/statisticAmounts/StatisticAmounts.js
@@ -1,32 +1,7 @@
-// import React, { Component } from 'react';
 import React from 'react';
 // import PropTypes from 'prop-types';
 import css from './StatisticAmounts.module.css';
 import { connect } from 'react-redux';
-
-// class StatisticAmounts extends Component {
-//   state = {};
-//   render() {
-//     console.log('props', this.props);
-//     console.log('costs', this.props.finance.costs);
-//     console.log('incomes', this.props.finance.incomes);
-//     return (
-//       <div className={css.StatisticAmountsContainer}>
-//         <ul className={css.AmountContainer}>
-//           <li className={css.AmountCost}>
-//             <p>Расходы: </p>
-//             <p className={css.TotalAmountCost}> - 2,098,457.00 грн.</p>
-//           </li>
-//           <span className={css.figure}></span>
-//           <li className={css.AmountIncome}>
-//             <p>Доходы: </p>
-//             <p className={css.TotalAmountIncome}> + 3,464,565.00 грн.</p>
-//           </li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
 
 const StatisticAmounts = () => (
   <div className={css.StatisticAmountsContainer}>
