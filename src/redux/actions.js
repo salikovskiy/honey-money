37997b2073dd7ed6b207d85d03f41421404616a3
VolkonovichD <volkonovich1989@gmail.com>
2020-02-06 13:44:57 +0200

    fix_new

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -7,24 +7,23 @@ export const fetchStart = () => ({
 
 export const getBalanceSuccess = value => ({
   type: Type.GET_BALANCE_SUCCESS,
-  payload: {value},
+  payload: { value },
 });
 
 export const getCostsSuccess = arr => ({
   type: Type.GET_COSTS_SUCCESS,
-  payload: {arr},
+  payload: { arr },
 });
 
 export const fetchError = error => ({
   type: Type.FETCH_ERROR,
-  payload: {error},
+  payload: { error },
 });
 
 export const addDateNow = date => ({
   type: Type.DATE_NOW,
-  payload: {date},
+  payload: { date },
 });
-
 
 export const signUp = newUser => {
   return (dispatch, getState) => {
